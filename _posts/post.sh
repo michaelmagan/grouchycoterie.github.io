b39144e23e386d15cbb4dd7f24c53002a28ab4c6
Michael Magan <michael@magan.info>
2020-07-13 08:36:58 -0500

    created script to automatically check spelling and grammar then commit and push post.

---
+++ _posts/post.sh
@@ -0,0 +1,16 @@
+echo 'Checking spelling.'
+aspell check $1
+
+echo 'Checking grammar.'
+gramma check $1
+
+echo 'Adding file to git.'
+git add $1
+
+echo 'Adding commit message.'
+git commit -m "Adding $1."
+
+echo 'Pushing to master.'
+git push origin master
+
+
